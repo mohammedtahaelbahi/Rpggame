55d87e4f4f9a2d87fda271d5c60a01115514ed1a
Taha <mohammedtaha.elbahi@usmba.ac.ma>
2026-02-11 23:50:24 +0100

    New enemy
    enemy skeleton

--- main.cpp
+++ main.cpp
@@ -1,76 +1,106 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
 int main() {
+
+
+
+
+    std::vector <int> yasin;
+    yasin.push_back(1);
+    yasin.push_back(2);
+    yasin.push_back(3);
+    yasin.push_back(4);
+    yasin.push_back(5);
+    for (int i=0;i<yasin.size();i++) {
+        std::cout <<yasin[i]<<"\n";
+    }
+
+
+
+
+
+
+
     // ------ Initiale-------
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
-    sf::RenderWindow window(sf::VideoMode(1280,720), "Rpg Game",sf::Style::Default,settings);
+    sf::RenderWindow window(sf::VideoMode(1920,1080), "Rpg Game",sf::Style::Default,settings);
 
 
     // ------ Initiale-------
+//----------------------------------------------------------------------------Load Skeleton
 
-    //-------Load---------
+    sf::Texture skeleton_texture;
+    sf::Sprite enemy_sprite;
+    if (skeleton_texture.loadFromFile("Assets/Skeleton/Textures/spritesheet.png")){
+        std::cout <<"Image enemy loaded\n";
+        enemy_sprite.setTexture(skeleton_texture);
+        enemy_sprite.setPosition(sf::Vector2f(400,600));
+        int XIndex{0}, YIndex{2};
 
+
+        enemy_sprite.setTextureRect(sf::IntRect(XIndex *64 ,YIndex * 64,64,64));
+
+        enemy_sprite.scale(sf::Vector2f(2,2));
+    }
+    else {
+        std::cout <<"Image enemy texture could not be loaded\n";
+    }
+//----------------------------------------------------------------------------Load Skeleton
+//----------------------------------------------------------------------------Load Player
     sf::Texture playerTexture;
     sf::Sprite playerSprite;
 
     if(playerTexture.loadFromFile("Assets/Player/Textures/spritesheet.png")) {
-        std::cout <<"Image player loaded\n";
         playerSprite.setTexture(playerTexture);
-        int XIndex{1}, YIndex{2};
+        int XIndex{0}, YIndex{3};
 
 
+        std::cout <<"Image player loaded\n";
         playerSprite.setTextureRect(sf::IntRect(XIndex *64 ,YIndex * 64,64,64));
 
         playerSprite.scale(sf::Vector2f(2,2));
     }
+    else {
+        std::cout <<"Image player texture could not be loaded\n";
+    }
+//----------------------------------------------------------------------------Load Player
 
 
-    //-------Load---------
-//main game loop
+//---------------------------------------------------------------------main game loop
     while (window.isOpen()) {
 
-        // -----------Update---------
+// -----------------------------------------------------------------------Update
 
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
-            /*if (event.type == event.KeyPressed) {
-                if (event.key.code == sf::Keyboard:: F) {
-
-                    //0,0
-
-                    sf::Vector2f position = playerSprite.getPosition();
-                    playerSprite.setPosition(position + sf::Vector2f(10,0));
-                }
-            } */
-
-
         }
         sf::Vector2f position = playerSprite.getPosition();
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            playerSprite.setPosition(position + sf::Vector2f(0.5,0));
+            playerSprite.setPosition(position + sf::Vector2f(1,0));
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-            playerSprite.setPosition(position + sf::Vector2f(-0.5,0));
+            playerSprite.setPosition(position + sf::Vector2f(-1,0));
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
-            playerSprite.setPosition(position + sf::Vector2f(0,-0.5));
+            playerSprite.setPosition(position + sf::Vector2f(0,-1));
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::
             S)) {
-            playerSprite.setPosition(position + sf::Vector2f(0,0.5));
+            playerSprite.setPosition(position + sf::Vector2f(0,1));
         }
 
-
-        // -----------Update---------
+//------------------------------------------------------------------------Update
 
 
         //-------Draw-----
         window.clear(sf::Color::Black);
         window.draw(playerSprite);
+        window.draw(enemy_sprite);
         window.display();
         //-------Draw-----
     }
